fba5c3301b862bf2661a509fa824264ed67f0fd8
Jørgen Juel <jorgen@creatur.no>
2014-03-14 17:18:56 +0100

    Endringer i footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,12 @@
 	</section>
 	<footer class="main-footer">
-		<p>Copyright &copy; 2014 Jørgen Juel</p>
+    <div class="info">
+      <h3>Praktisk info</h3>
+      <p>Her vil det komme litt praktisk info om meg</p>
+    </div>
+    <div class="copyright">
+  		<p>Copyright &copy; 2014 Jørgen Juel</p>
+    </div>
 	</footer>
 </body>
 </html>
